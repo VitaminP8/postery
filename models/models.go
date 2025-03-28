ee05f9d12fc5db96d413b7c965afaa2ec9b94884
Alexey <alexey.zhukovich@gmail.com>
2025-03-28 15:59:14 +0300

    add DB models

---
+++ models/models.go
@@ -0,0 +1,30 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique"`
+	Email    string `gorm:"unique"`
+	Password string
+	Posts    []Post    `gorm:"foreignkey:UserID"`
+	Comments []Comment `gorm:"foreignkey:UserID"`
+}
+
+type Post struct {
+	gorm.Model
+	Title            string
+	Content          string
+	CommentsDisabled bool
+	UserID           uint
+	Comments         []Comment `gorm:"foreignkey:PostID"`
+}
+
+type Comment struct {
+	gorm.Model
+	Content  string
+	PostID   uint
+	UserID   uint
+	ParentID uint
+	Children []Comment `gorm:"foreignkey:ParentID"`
+}
